790938e1522fe077b4dd29ecf0471222aaecd558
YuikoIto <toffywing@gmail.com>
2020-06-26 18:54:09 +0900

    make timeline

--- lib/Home.dart
+++ lib/Home.dart
@@ -10,7 +10,27 @@ class _HomeState extends State<Home>{
   Widget build(BuildContext context){
     return Scaffold(
       body: Center(
-        child: Text("Home"),
+        child: ListView(
+          children: List.generate(8, (index) {
+        return InkWell(
+          onTap:(){},
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text("Index ユーザー $index"),
+                leading: Image.asset(
+                  "assets/sample/sample$index.png",
+                  width: 30.0,
+                  height: 30.0,
+                ),
+                subtitle: Text("サンプル文\nサンプル"),
+              ),
+              SizedBox(height: 20.0,),
+              Divider(height: 5.0,),
+            ],
+          ),
+        );
+       })),
       ),
     );
   }

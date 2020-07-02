3f884ab7cd73025d60e7da6777eb2bbf2362abff
YuikoIto <toffywing@gmail.com>
2020-07-02 20:05:55 +0900

    develop googlesignin

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:twitterclone/screens/chat_screen.dart';
-import 'package:twitterclone/widgets/Home.dart';
-import 'package:twitterclone/widgets/KeyWord.dart';
-import 'package:twitterclone/widgets/Notifications.dart';
+import 'package:twitterclone/pages/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,68 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FlutterTwitter',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
-        accentColor: Colors.blue,
-        primarySwatch: Colors.blue,
-      ), 
-      home: new MyHomePage(),
+      title: 'FlutterShare',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          primaryColor: Colors.blueAccent, accentColor: Colors.deepPurple),
+      home: Home(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int currentIndex = 0;
-
-  List tabItemWidget =[
-    Home(),
-    KeyWord(),
-    Notifications(),
-    ChatScreen(),
-  ];
-  @override
-  Widget build(BuildContext context){
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("ホーム"),
-      ),
-      body: new Center(
-        child: tabItemWidget[currentIndex],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        onTap: _onTaped,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home, color: Colors.blue,),
-            title: new Text(''),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.search, color: Colors.blue,),
-            title: new Text(''),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.notifications, color: Colors.blue,),
-            title: new Text(''),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.mail, color: Colors.blue,),
-            title: new Text(''),
-          ),
-        ]),
-    );
-  }
-
-  _onTaped(int index){
-    setState(() {
-      currentIndex = index;
-    });
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+final GoogleSignIn googleSignIn = GoogleSignIn();
 
 class Home extends StatefulWidget {
   @override
@@ -7,8 +10,75 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  @override
+  void initState() {
+    super.initState();
+    googleSignIn.onCurrentUserChanged.listen((account) {
+      if (account != null) {
+        print('すでにログインしています！!: $account');
+        setState(() {
+          isAuth = true;
+        });
+      } else {
+        setState(() {
+          isAuth = false;
+        });
+      }
+    });
+  }
+
+  login() {
+    googleSignIn.signIn();
+  }
+
+  Widget buildAuthScreen() {
+    return Text('Authenticated');
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+              Theme.of(context).primaryColor,
+              Theme.of(context).accentColor
+            ])),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Twitterclone',
+              style: TextStyle(
+                  fontFamily: "Signatra", fontSize: 90.0, color: Colors.white),
+            ),
+            GestureDetector(
+              onTap: login,
+              child: Container(
+                width: 260.0,
+                height: 60.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          AssetImage('assets/sample/google_signin_button.png'),
+                      fit: BoxFit.cover),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
   }
 }
